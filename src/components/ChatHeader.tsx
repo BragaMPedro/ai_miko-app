d348eee87c3bee269a1ed229ac022d054b0e7715
Pedro Braga Magalhães <111090976+BragaMPedro@users.noreply.github.com>
2024-06-01 23:42:04 +0000

    header parcial update

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,4 +1,5 @@
 import configIcon from "@/assets/icons/config-icon.svg";
+import historyIcon from "@/assets/icons/history-icon.svg"
 import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
 
@@ -8,10 +9,13 @@ interface ChatHeaderProps{
 
 export const ChatHeader = ({setShowModal}: ChatHeaderProps) => {
    return (
-      <header className="container fixed top-0 w-full bg-transparent">
-         <div className="max-w-5xl text-left py-4">
+      <header className="container fixed left-0 top-0 z-10 w-fit bg-transparent">
+         <div className="p-4 space-y-4 flex flex-col items-center">
             <button onClick={()=>setShowModal(true)}>
                <Image src={configIcon} alt="Ícone de engrenagem simbolizando configurações" />
+            </button>
+            <button>
+               <Image src={historyIcon} alt="icone de caderno simbolizando histórico da conversa" />
             </button>
          </div>
       </header>

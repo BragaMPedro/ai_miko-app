425f44513741b1fe84112b6339703af01c538eba
BragaMPedro <pedrobmagalhaes95@gmail.com>
2024-08-12 18:33:10 -0300

    feat: textarea dinamic height

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,41 +1,62 @@
-import { ChangeEvent, Dispatch, FormEvent, SetStateAction } from 'react';
-interface ChatInputProps{
-    message: string,
-    setMessage: Dispatch<SetStateAction<string>>,
-    loading: boolean
-    submit: ()=>void
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
+interface ChatInputProps {
+   message: string;
+   setMessage: Dispatch<SetStateAction<string>>;
+   loading: boolean;
+   submit: () => void;
 }
 
-export const ChatInput = ({message, setMessage, loading,  submit}: ChatInputProps) => {
-  const disabled = message === "" || loading;
+export const ChatInput = ({ message, setMessage, loading, submit }: ChatInputProps) => {
+   const disabled = message === "" || loading;
 
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(e.target.value);
-  };
+   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const input = e.target.value;
+      setMessage(input);
+   };
 
-  const handleSubmit = (e?: FormEvent<HTMLFormElement>) => {
-    e?.preventDefault();    
-    submit();  //Função para enviar a mensagem para o chatbot
-    setMessage(''); // Limpar o campo de input
-    
-  };
+   const inputHeightSetter = () => {
+      let currentHeight;
 
-  return (
-    <form className="fixed bottom-0 left-0 flex items-center space-x-4 w-full max-w-5xl p-4" onSubmit={handleSubmit} >
-      <textarea
-        id='msg-input'
-        name='msg-input'
-        maxLength={500}
-        value={message}
-        inputMode='text'
-        onKeyUp={(e)=>{ if(e.key === "Enter") handleSubmit() }}
-        onChange={handleChange}
-        placeholder="Digite sua mensagem..."
-        className="flex-grow p-2 rounded-md text-white border border-gray-300"
-      />
-      <button type="submit" disabled={disabled} className={`${disabled && "btn-disabled"} btn btn-square btn-primary cursor-pointer text-white px-4 py-2 rounded-md hover:brightness-75 transition-all duration-200`}>
-        Enviar
-      </button>
-    </form>
-  );
+      if (message && message.match(/\n/g)?.length) {
+         currentHeight = message.match(/\n/g)!.length * 44;
+
+         return currentHeight + "px";
+      }
+   };
+
+   const handleSubmit = () => {
+      submit(); //Função para enviar a mensagem para o chatbot
+      setMessage(""); // Limpar o campo de input
+   };
+
+   return (
+      <div className="fixed bottom-0 flex flex-col self-center h-fit z-10 w-full max-w-5xl sm:mb-6">
+         <label
+            className={`textarea textarea-bordered flex grow items-end pl-8 pr-2 gap-2 ${
+               inputHeightSetter() != "44px" ? "rounded-badge" : "rounded-full"
+            } shadow-md shadow-black/20`}>
+            <textarea
+               id="msg-input"
+               name="msg-input"
+               rows={5}
+               maxLength={800}
+               value={message}
+               inputMode="text"
+               onChange={handleChange}
+               style={{ height: inputHeightSetter() }}
+               placeholder="Digite sua mensagem..."
+               className="grow w-full text-white bg-transparent h-10 resize-none max-h-32 transition-all focus:outline-none"
+            />
+            <button
+               type="button"
+               onClick={handleSubmit}
+               disabled={disabled}
+               className={`${
+                  disabled && "btn-disabled"
+               } btn btn-primary relative bottom-0 rounded-full text-white hover:brightness-75 transition-all duration-200`}>
+               Enviar
+            </button>
+         </label>
+      </div>
+   );
 };
